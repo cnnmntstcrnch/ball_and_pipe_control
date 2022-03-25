c4c5bf3c8518b83827c4509be550036e44a0e1e7
Jack <bellja59@students.rowan.edu>
2022-03-25 12:28:35 -0400

    Configured the PWM

--- set_pwm.m
+++ set_pwm.m
@@ -11,10 +11,15 @@ function action = set_pwm(device, pwm_value)
 % Modified by: YOUR NAME and DATE
 
 %% Force PWM value to be valid
-% pwm_value = % Bound value to limits 0 to 4095
+if(pwm_value < 0)
+    pwm_value = 0;
+elseif(pwm_value > 4095)
+    pwm_value = 4095;
+end
+% Bound value to limits 0 to 4095
 
 %% Send Command
-% action = % string value of pwm_value
+action = int2str(pwm_value);% string value of pwm_value
 % use the serialport() command options to change the PWM value to action
 
 end
